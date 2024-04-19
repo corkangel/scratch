fd7060955b0353092ffd46b83c29a3ad3852d1cc
mrrileyx <mrrileyx@gmail.com>
2024-04-19 09:10:53 -0700

    remove

--- t.py
+++ t.py
@@ -1,34 +1,33 @@
+
+# t_model_conv_layer_backwards
 import torch
 from torch import nn
 
-batch_size = 2
-input_channels = 5
-output_features = 2
+batch_size = 1
+input_channels = 1
+output_features = 1
 
-input_data = ((torch.arange(0, batch_size * 80) * 0.1).pow(1.5) * 0.1).reshape(batch_size,input_channels, 4, 4)
+input_data = ((torch.arange(0, batch_size * 25) * 0.1).pow(1.5) * 0.1).reshape(batch_size, input_channels, 5, 5)
 
-conv_layer1 = nn.Conv2d(input_channels, output_features, kernel_size=3, stride=2, padding=1)
+conv_layer1 = nn.Conv2d(input_channels, output_features, kernel_size=3, stride=2, padding=0)
 conv_layer1.weight.data.fill_(.1)
 conv_layer1.bias.data.fill_(.1)
 mid = conv_layer1(input_data)
-print(f"mid shape: {mid.shape}")
-print("mid", mid)
+print(f"mid shape: {mid.shape} 0:{mid[0][0][0][0]} 1:{mid[0][0][0][1]} ")
 
+print(f"conv_layer1.weight: {conv_layer1.weight}")
+print(f"conv_layer1.weight.shape: {conv_layer1.weight.shape}")
 
-conv_layer2 = nn.Conv2d(2, 1, kernel_size=2, stride=2, padding=0)
-conv_layer2.weight.data.fill_(.1)
-conv_layer2.bias.data.fill_(.1)
-output = conv_layer2(mid)
-print(f"output shape: {output.shape}")
-print("output", output)
+softmax = nn.Softmax(dim=1)
+smo = softmax(mid)
+print ("mid", mid)
+print ("smo", smo)
 
-mse = nn.MSELoss()
-
-target = torch.ones(batch_size, 1, 1, 1)
-loss = mse(output, target);
+target = torch.tensor([1,0])
+loss_fn = nn.CrossEntropyLoss()
+loss = loss_fn(mid.squeeze(), target)
 loss.backward()
 
 print(f"loss: {loss}")
-
-#print(f"Weights gradient shape: {conv_layer1.weight.grad.shape}")
-#print(f"Bias gradient shape: {conv_layer1.bias.grad.shape}")
+print ("conv_layer1.weight.grad", conv_layer1.weight.grad)
+print(f"conv_layer1.weight: {conv_layer1.weight}")
--- tt.py
+++ tt.py
@@ -1,34 +1,52 @@
+# t_model_conv_layer_backwards1
+
 import torch
-from torch import nn
+import torch.nn as nn
+import torch.optim as optim
 
-batch_size = 6
-input_channels = 5
-output_features = 2
+# Create a single 3x8x8 image
+input = (torch.arange(0., 192.)*0.1).reshape(1, 3, 8, 8)
 
-input_data = ((torch.arange(0, 480) * 0.1).pow(1.5) * 0.1).reshape(batch_size,input_channels, 4, 4)
+# Create a Conv2d layer
+conv = nn.Conv2d(3, 10, kernel_size=3, stride=1, padding=1)
+conv.weight.data.fill_(0.1)
+conv.bias.data.fill_(0.1)
 
-print (input_data)
+# Pass the image through the Conv2d layer
+output = conv(input)
 
-conv_layer1 = nn.Conv2d(input_channels, output_features, kernel_size=3, stride=2, padding=1)
-conv_layer1.weight.data.fill_(.01)
-conv_layer1.bias.data.fill_(.0)
-mid = conv_layer1(input_data)
-print(f"mid shape: {mid.shape} mid: { mid}")
+# Register a hook on the output to print the gradients
+output.register_hook(lambda grad: print("Gradients on the activations:", grad.shape, grad[0][0]))
 
-conv_layer2 = nn.Conv2d(2, 1, kernel_size=2, stride=2, padding=0)
-conv_layer2.weight.data.fill_(.1)
-conv_layer2.bias.data.fill_(.1)
-output = conv_layer2(mid)
-print(f"output shape: {output.shape} 0:{output[0][0][0][0]}")
+# Create a target tensor
+target = torch.tensor([9])
 
-mse = nn.MSELoss()
+# Create a CrossEntropyLoss
+criterion = nn.CrossEntropyLoss()
 
-target = torch.ones(6, 1, 1, 1)
-loss = mse(output, target);
+print("Output shape", output.view(1, -1).shape)
+print("Output ", output[0][0])
+
+# Compute the loss
+loss = criterion(output.view(1, -1), target)
+print ("loss:", loss)
+
+# Zero the gradients
+conv.zero_grad()
+
+# Backward pass
 loss.backward()
 
-print(f"loss: {loss}")
+# Print the gradients
+for name, param in conv.named_parameters():
+    print("Params Gradients", name, param.grad[0])
 
-print(f"Weights gradient shape: {conv_layer1.weight.grad.shape}")
-print(f"Bias gradient shape: {conv_layer1.bias.grad.shape}")
+print ("Weights Before:", conv.weight.data[0])
 
+# Create an optimizer
+optimizer = optim.SGD(conv.parameters(), lr=0.01)
+
+# Update the weights
+optimizer.step()
+
+print ("Weights After:", conv.weight.data[0])
